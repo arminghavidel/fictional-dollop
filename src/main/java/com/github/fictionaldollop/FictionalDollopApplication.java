3c4d119aa488ca135acd8157ddf35060cf998d66
armin.ghavidel <armin.ghavidel@snapppay.ir>
2023-12-28 09:32:48 +0330

    Batman! (this commit has no parents)

---
+++ src/main/java/com/github/fictionaldollop/FictionalDollopApplication.java
@@ -0,0 +1,13 @@
+package com.github.fictionaldollop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FictionalDollopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FictionalDollopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/fictionaldollop/FictionalDollopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fictionaldollop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FictionalDollopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
